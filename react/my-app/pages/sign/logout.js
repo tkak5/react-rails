842ba32cf17a91fbeab8fbec7a3fc3dda4069a37
三輪貴陽 <tkak@sanrinkyounoMBP.AirPort>
2022-01-23 16:46:40 +0900

    first

---
+++ react/my-app/pages/sign/logout.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+
+export default function Logout () {
+    return (
+        <>
+            <p>succese logout!</p>
+        </>
+    )
+}
