86aa2eb368ecc3009c217efa39b67e76784bc6a2
三輪貴陽 <tkak@sanrinkyounoMBP.AirPort>
2022-01-23 23:36:26 +0900

    second

--- rails/my-app/config/routes.rb
+++ rails/my-app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'home/index'
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
 end
